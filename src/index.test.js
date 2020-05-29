715c57a4f4105e2595e59f242d64189d5ad97439
Zsolt Gömöri <zsgomoridev@icloud.com>
2020-05-29 10:40:24 +0200

    Add base toggle button, set testing environment up.

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,59 @@
-import ExampleComponent from ".";
+import React from "react";
+import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
+import { fireEvent, render } from "@testing-library/react";
+import ReactThemeToggleButton from "./index";
 
-describe("ExampleComponent", () => {
-  it("is truthy", () => {
-    expect(ExampleComponent).toBeTruthy();
+describe("ReactThemeToggleButton", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<ReactThemeToggleButton isDark onChange={() => {}} />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  describe("when the `isDark` prop is true", () => {
+    it("sets the label's title and aria-label attributes to `Activate light mode`", () => {
+      const { getByTitle } = render(<ReactThemeToggleButton isDark />);
+
+      expect(getByTitle(/activate/i).title).toEqual("Activate light mode");
+      expect(getByTitle(/activate/i).getAttribute("aria-label")).toEqual(
+        "Activate light mode"
+      );
+    });
+  });
+
+  describe("when the `isDark` prop if false", () => {
+    it("sets the label's title and aria-label attributes to `Active dark mode`", () => {
+      const { getByTitle } = render(<ReactThemeToggleButton isDark={false} />);
+
+      expect(getByTitle(/activate/i).title).toEqual("Activate dark mode");
+      expect(getByTitle(/activate/i).getAttribute("aria-label")).toEqual(
+        "Activate dark mode"
+      );
+    });
+  });
+
+  describe("when the toggle button is pressed", () => {
+    let onChangeSpy;
+
+    beforeEach(() => {
+      onChangeSpy = jest.fn();
+    });
+
+    afterEach(() => {
+      onChangeSpy.mockReset();
+    });
+
+    it("fires the passed function", () => {
+      const { getByTitle } = render(
+        <ReactThemeToggleButton isDark onChange={onChangeSpy} />
+      );
+
+      fireEvent.click(getByTitle(/activate/i));
+
+      expect(onChangeSpy).toHaveBeenCalled();
+    });
   });
 });
